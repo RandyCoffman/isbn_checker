363c0b52012a58d865bf9297820120ae9e63172e
RandyCoffman <randy.coffman@wvjc.edu>
2018-04-17 14:09:39 -0400

    working tests to remove dashes

--- isbn_validation_test.rb
+++ isbn_validation_test.rb
@@ -7,14 +7,23 @@ class Random_pairs_test < Minitest::Test
 		assert_equal(true, true)
 	end
 
-	def test_no_space
+	def test_no_spaces
 		nothing = remove_spaces("1 2 3")
 		assert_equal("123", nothing)
 	end
 
-	def test_no_space2
+	def test_no_spaces2
 		nothing = remove_spaces("4 5 9 10")
 		assert_equal("45910", nothing)
 	end
 
+	def test_no_dashes
+		nothing = remove_dashes("4-5-9-10")
+		assert_equal("45910", nothing)
+	end
+
+	def test_no_dashes2
+		nothing = remove_dashes("1-2-3")
+		assert_equal("123", nothing)
+	end
 end
