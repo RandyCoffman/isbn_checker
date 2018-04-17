f192e730bcfa208fd44781c3c0493affed10db5c
RandyCoffman <randy.coffman@wvjc.edu>
2018-04-17 13:45:13 -0400

    first commit

---
+++ isbn_validation.rb
@@ -0,0 +1,4 @@
+def remove_spaces_and_dashes
+	gsub!(" ", "")
+	gsub!("-", "")
+end
---
+++ isbn_validation_test.rb
@@ -0,0 +1,14 @@
+require "minitest/autorun"
+require_relative "isbn_validation.rb"
+
+class Random_pairs_test < Minitest::Test
+
+	def test_boolean
+		assert_equal(true, true)
+	end
+
+	def test_no_space
+		
+	end
+
+end
