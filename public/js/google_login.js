5581c527fd258b8f03c4c2d3e4b28008e75ac14c
RandyCoffman <randy.coffman@wvjc.edu>
2018-05-03 10:18:25 -0400

    login is working great, testing logging out now.

--- public/js/google_login.js
+++ public/js/google_login.js
@@ -1,14 +1,11 @@
 function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
-  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log('Name: ' + profile.getName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    var profile = googleUser.getBasicProfile();
+  		console.log('Name: ' + profile.getName());
+  		console.log('Image URL: ' + profile.getImageUrl());
+        console.log("Succesfully Singed in!!!");
 }
 
 function signOut() {
-	var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
+	document.location.href = "https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=https://scotts-isbn-validator.herokuapp.com/";
       console.log('User signed out.');
-    });
 }
