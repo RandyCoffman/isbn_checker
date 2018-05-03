5ea66752b5371ff9027bfa9f679e3942e60f5b1d
RandyCoffman <randy.coffman@wvjc.edu>
2018-05-03 10:20:24 -0400

    further logout testing

--- public/js/google_login.js
+++ public/js/google_login.js
@@ -6,6 +6,6 @@ function onSignIn(googleUser) {
 }
 
 function signOut() {
-	document.location.href = "https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=https://scotts-isbn-validator.herokuapp.com/";
+	document.location.href = "https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=https://scotts-isbn-validator.herokuapp.com";
       console.log('User signed out.');
 }
